67ba71bcfd0146b6f494ab6b2b54ab99dc73e2aa
Lord-Gusarov <gustavo.hc18@gmail.com>
2020-09-16 14:36:32 -0400

    Still trying to make 4 betty conforming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
-/*Greeting the World*/
+/**
+ *main - Greeting the World
+ *
+ *Return: 0 is no error
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
