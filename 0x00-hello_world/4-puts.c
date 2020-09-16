d98c5bda46b05f7764bc4d38d01d0c39f029ff03
Lord-Gusarov <gustavo.hc18@gmail.com>
2020-09-16 13:59:41 -0400

    removing extra line since puts already places a trailing new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,6 @@
 /*Greeting the World*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,8 +2,8 @@
 
 /*Practicing the use of 'printf' */
 
-int main()
+int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return 0;
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
