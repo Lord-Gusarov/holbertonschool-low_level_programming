de01d579b1b2b062cde5ab45cdd959aa92cde653
Lord-Gusarov <gustavo.hc18@gmail.com>
2020-09-16 15:22:26 -0400

    making all files betty_style conforming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  *main-Printing to stander error
- *Retunr:1 in this case 1 is the intended succesfull output
+ *Return: 1 in this case 1 is the intended succesfull output
  */
 int main(void)
 {
-  fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-  return (1);
+fprintf(stderr, "and that piece of art is useful\" - ");
+fprintf("Dora Korpar, 2015-10-19\n");
+return (1);
 }
