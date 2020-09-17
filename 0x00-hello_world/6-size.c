4c40929da7310c6f2e2d28bd07cad7e38e08b5b2
Lord-Gusarov <gustavo.hc18@gmail.com>
2020-09-16 20:01:38 -0400

    betty_styles for 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+
 /**
  *main-Printing 32 and 64 systemsn specific sizes for common types
- *Return: 0 if no errors
+ *
+ *Return: always 0 (succes)
  */
 int main(void)
 {
 printf("Size of a char: %lu bytes(s)\n", sizeof(char));
 printf("Size of an int: %lu bytes(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %lu byte(s)\n", sizeof(float));
 return (0);
 }
