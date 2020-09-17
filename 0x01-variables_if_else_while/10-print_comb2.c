1a18e8c16fd3a83cbe58e64f4a28d807de1d4420
Lord-Gusarov <gustavo.hc18@gmail.com>
2020-09-17 15:39:05 -0400

    Ading task 10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *main - the number 00 to 99 separeted by comma and a space
+ *
+ *Return: alway 0
+ */
+int main(void)
+{
+	int d1, d2;
+
+	for(d1 = 0; d1 < 10; d1++)
+	{
+		for(d2 = 0; d2 < 10; d2++)
+		{
+			putchar(d1 + '0');
+			putchar(d2 + '0');
+			if ((d1*10 + d2) < 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
