37b928673dee8102d9fa1c0b1bcda74a87318513
Lord-Gusarov <gustavo.hc18@gmail.com>
2020-09-17 15:41:03 -0400

    Ading task 10

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -9,13 +9,13 @@ int main(void)
 {
 	int d1, d2;
 
-	for(d1 = 0; d1 < 10; d1++)
+	for (d1 = 0; d1 < 10; d1++)
 	{
-		for(d2 = 0; d2 < 10; d2++)
+		for (d2 = 0; d2 < 10; d2++)
 		{
 			putchar(d1 + '0');
 			putchar(d2 + '0');
-			if ((d1*10 + d2) < 99)
+			if ((d1 * 10 + d2) < 99)
 			{
 				putchar(',');
 				putchar(' ');
