33eeedfd35abe31aeab27a438d86db7a8137ba72
Lord-Gusarov <gustavo.hc18@gmail.com>
2020-09-17 18:14:56 -0400

    Adding task 13 (advanced)

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ *main - prints unique  possible combinations of two two-digits
+ *
+ *Return: alway 0
+ */
+int main(void)
+{
+	int d1, d2, d3 , d4;
+
+	int end1, end2;
+
+	for (d1 = 0; d1 < 10; d1++)
+	{
+		for (d2 = 0; d2 < 10; d2++)
+		{
+			for (d3 = d1 ; d3 < 10; d3++)
+			{
+				for (d4 = d2 + 1 ; d4 < 10; d4++)
+				{
+				putchar(d1 + '0');
+				putchar(d2 + '0');
+				putchar(' ');
+				putchar(d3 + '0');
+				putchar(d4 + '0');
+
+				end1 = d1 *10 + d2;
+				end2 = d3 * 10 + d4;
+				if ( ! ((end1 == 98) && (end2 == 99)))
+				{
+					putchar(',');
+					putchar(' ');
+
+				}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
