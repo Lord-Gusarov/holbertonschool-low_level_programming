d4d2ed445fe98d677cfb6cb0c93c1180143ce1fe
Lord-Gusarov <gustavo.hc18@gmail.com>
2020-09-18 19:49:27 +0000

    VA- not using array

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
-
 /**
  *main-Printing to stander error
  *Return: 1 in this case 1 is the intended succesfull output
  */
 int main(void)
 {
-char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-write(2, s, strlen(s));
-
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
