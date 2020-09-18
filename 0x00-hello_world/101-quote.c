4bff81403b1c76c9f9b4068a6919a173eb9051c6
Lord-Gusarov <gustavo.hc18@gmail.com>
2020-09-18 19:17:19 +0000

    pushing from vagrant the 101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
 /**
  *main-Printing to stander error
  *Return: 1 in this case 1 is the intended succesfull output
  */
 int main(void)
 {
-echo "and that piece of art is useful\" - " 2>;
-fprintf(stderr, "Dora Korpar, 2015-10-19\n");
+char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+write(2, s, strlen(s));
+
 return (1);
 }
