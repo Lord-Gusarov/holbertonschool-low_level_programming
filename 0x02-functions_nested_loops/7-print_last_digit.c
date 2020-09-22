42f866df8d066cfd012e8574a32ad150fb51f18e
Lord-Gusarov <gustavo.hc18@gmail.com>
2020-09-22 10:05:20 -0400

    fixing issue when given the INT_MIN

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,12 +7,10 @@
  */
 int print_last_digit(int n)
 {
-	int d = 0;
-
+	n = n % 10;
 	if (n < 0)
 		n = n * -1;
-	d = n % 10;
-	_putchar(d + '0');
+	_putchar(n + '0');
 
-	return (d);
+	return (n);
 }
