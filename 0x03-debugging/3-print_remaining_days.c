eafe906429dae4ab31f96d7e82c94073fad4bcb9
Lord-Gusarov <gustavo.hc18@gmail.com>
2020-09-22 14:42:13 -0400

    fixing the off by one error near the leapt day

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -15,9 +15,11 @@ void print_remaining_days(int month, int day, int year)
 	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
 	{
 		/*if (month >= 2 && day > 60)*/
-		if (day > 60)
+		if (month >= 2 && day >= 60)
 		{
 			day++;
+			if (month == 2 && day == 61)
+				day--;
 		}
 
 		printf("Day of the year: %d\n", day);
