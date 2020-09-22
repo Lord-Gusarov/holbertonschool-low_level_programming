f8966bd1483cfa674407c5995e6c4cbe167c57be
Lord-Gusarov <gustavo.hc18@gmail.com>
2020-09-22 07:16:49 -0400

    betty conforming

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  *main - computes and prints the sum of all the
  *multiples of 3 and 5 below 1024
@@ -7,12 +8,14 @@
  */
 int main(void)
 {
-	int i, sum = 0, limit = 1024;
+	int i;
+	int sum = 0;
+	int limit = 1024;
 
 	for (i = 0; i < limit; i++)
 		if (i % 3 == 0 || i % 5 == 0)
 			sum += i;
 	printf("%i\n", sum);
-	
+
 	return (0);
 }
