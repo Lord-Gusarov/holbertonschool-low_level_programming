11db6b5731ff514981c859c5731b95d4e0b0f861
Lord-Gusarov <gustavo.hc18@gmail.com>
2020-09-22 06:52:57 -0400

    typo in Return tagf of the comments

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
  *_isalpha - Determines if a given character is an Alphabetic chracter
  *@c: character to be tested
  *
- *Retuen: 1 if @c isa a letter, o if otherwise
+ *Return: 1 if @c isa a letter, o if otherwise
  */
 int _isalpha(int c)
 {
