6977b6b6cac5fb640dcb3b8e56257ff33a6e3b1f
Lord-Gusarov <gustavo.hc18@gmail.com>
2020-09-22 22:46:00 -0400

    making fibonacci betty compliant

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,24 +5,24 @@
  *
  *Return: always 0
  */
-int main()
+int main(void)
 {
-    int count;
-    long int f1 = 1;
-    long int f2 = 2;
-    long int next;
-    int flimit = 50;
+	int count;
+	long int f1 = 1;
+	long int f2 = 2;
+	long int next;
+	int flimit = 50;
 
-    for (count = 2; count <= flimit; count++)
-    {
-        next = f1 + f2;
-        f1 = f2;
-        f2 = next;
-        printf("%lu", next);
-        if (count < flimit)
-            printf(", ");
-    }
-    printf("\n");
+	for (count = 2; count <= flimit; count++)
+	{
+		next = f1 + f2;
+		f1 = f2;
+		f2 = next;
+		printf("%lu", next);
+		if (count < flimit)
+			printf(", ");
+	}
+	printf("\n");
 
-    return 0;
+	return (0);
 }
