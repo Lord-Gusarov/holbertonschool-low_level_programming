af3e08fba08e90c099e46a6367428ec9eb018eef
Lord-Gusarov <gustavo.hc18@gmail.com>
2020-09-22 22:40:53 -0400

    First commit Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ *main - prints the first 50 fibonacci numbers starting with 1 and 2
+ *
+ *Return: always 0
+ */
+int main()
+{
+    int count;
+    long long int f1 = 1;
+    long long int f2 = 2;
+    long long int next;
+    int flimit = 50;
+
+    for (count = 2; count <= flimit; count++)
+    {
+        next = f1 + f2;
+        f1 = f2;
+        f2 = next;
+        printf("%llu", next);
+        if (count < flimit)
+            printf(", ");
+    }
+    printf("\n");
+
+    return 0;
+}
