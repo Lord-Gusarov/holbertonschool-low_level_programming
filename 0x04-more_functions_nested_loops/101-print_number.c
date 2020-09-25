aefc6048f2da79a25b5be93c89fc3154fd67cada
Lord-Gusarov <gustavo.hc18@gmail.com>
2020-09-24 23:46:37 -0400

    adding task 101 bonus

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ *print_number - print any integer character by character
+ *@n: integer to be printed
+ *
+ *Return: -nothing-
+ */
+void print_number(int n)
+{
+	int c = 1;
+	int h;
+
+	while (n / (c * 10) != 0)
+		c = c * 10;
+	if (n < 0)
+		_putchar('-');
+	while (c >= 1)
+	{
+		h = n / c;
+		if (h < 0)
+			_putchar(-h + '0');
+		else
+			_putchar(h + '0');
+		n -= h * c;
+		c = c / 10;
+	}
+
+	_putchar('\n');
+}
