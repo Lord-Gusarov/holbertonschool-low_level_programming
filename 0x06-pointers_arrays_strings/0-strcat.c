5410dd57284fdf3828ab9a4d4bffefd4cf1fc0d1
Lord-Gusarov <2367@holbertonschool.com>
2020-10-01 13:37:22 -0400

    Making it betty style compliant

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,7 +3,7 @@
 /**
  *_strcat - concatenates two strings
  *@dest: string we adding to
- *@scr: string being added
+ *@src: string being added
  *
  *Return: pointer to the resulting string
  */
@@ -12,9 +12,9 @@ char  *_strcat(char *dest, char *src)
 	int size_dest;
 	int size_src;
 
-	for(size_dest = 0; dest[size_dest] != '\0'; size_dest++)
+	for (size_dest = 0; dest[size_dest] != '\0'; size_dest++)
 		;
-	for(size_src = 0; src[size_src] != '\0'; size_src++)
+	for (size_src = 0; src[size_src] != '\0'; size_src++)
 		dest[size_dest + size_src] = src[size_src];
 	dest[size_dest + size_src] = '\0';
 
