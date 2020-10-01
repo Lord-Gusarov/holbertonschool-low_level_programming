a7273e7768649975c8c085632585cbf9351c59a7
Lord-Gusarov <2367@holbertonschool.com>
2020-10-01 15:46:28 -0400

    making all empty elements up to n be NULL but with just 'for'

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,11 +14,9 @@ char  *_strncpy(char *dest, char *src, int n)
 
 	for (i = 0; src[i] != '\0' && n > i; i++)
 		dest[i] = src[i];
-	while (i < n)
-	{
+
+	for (; i < n; i++)
 		dest[i] = '\0';
-		i++;
-	}
 
 	return (dest);
 }
