43955dd6ac4d1064443ceb455a55f7185654b0f7
Lord-Gusarov <2367@holbertonschool.com>
2020-10-02 13:29:03 -0400

    first commit task 12 (adv)

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+/**
+ *print_times_table - prints the n times table for 0 < n < 16
+ *@n: the last table to print starting with 0 ending inm 'n'
+ */
+void print_times_table(int n)
+{
+	if (n < 0 || n > 15)
+		return;
+	int i, ii num;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (ii = 0; ii <= n; ii++)
+		{
+			num = i * ii;
+			if (num > 99)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(num / 100 + '0');
+				num = num - (num / 100) * 100;
+				_putchar((num / 10) + '0');
+				_putchar((num % 10) + '0');
+			}
+			else if (num > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((num / 10) + '0');
+				_putchar((num % 10) + '0');
+			}
+			else if (ii != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(num + '0');
+			}
+			else
+				_putchar('0');
+		}
+		_putchar('\n');
+	}
+}
