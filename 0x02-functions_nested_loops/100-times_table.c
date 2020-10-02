2e81b13b69f46234616d608684e64ea5ff552d98
Lord-Gusarov <2367@holbertonschool.com>
2020-10-02 13:40:04 -0400

    removing 'return' to exit fucntion

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,8 +5,8 @@
  */
 void print_times_table(int n)
 {
-	if (n < 0 || n > 15)
-		return;
+	if (n >= 0 && n <= 15)
+	{
 	int i, ii, num;
 
 	for (i = 0; i <= n; i++)
@@ -43,5 +43,6 @@ void print_times_table(int n)
 				_putchar('0');
 		}
 		_putchar('\n');
+	}
 	}
 }
