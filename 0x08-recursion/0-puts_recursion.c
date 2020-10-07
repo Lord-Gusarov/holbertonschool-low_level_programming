51d8e34ebbf3431d020513c45cab37b19b60c0d6
Khalah Jones-Golden <khasan222@gmail.com>
2020-10-07 07:23:36 -1000

    changing the end of recursion output to '\n', before it was a simple 'return'

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,9 +8,12 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
-		_putchar(s[0]);
+	if(*s)
+	{
+		_putchar(*s);
+        _puts_recursion(s + 1);
+	}
 	else
-		return;
-	_puts_recursion(++s);
+		_putchar('\n');
+	
 }
