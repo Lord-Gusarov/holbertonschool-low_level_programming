05d785b27ec01da455fc2a737e9f9669ea591dc0
Lord-Gusarov <2367@holbertonschool.com>
2020-10-15 12:15:28 -0400

    making task 0 Betty style compliant, cutting lone over 80 chars and adding blank line after variable declarations

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,12 +5,13 @@
  * malloc_checked - allocates memory using malloc
  * @b: amount of bytes to separate
  *
- * Return: f malloc fails, the malloc_checked function should cause normal process
- * termination with a status value of 98
+ * Return: f malloc fails, the malloc_checked function
+ * should cause normal process termination with a status value of 98
  */
 void *malloc_checked(unsigned int b)
 {
 	void *m;
+
 	if (b == 0)
 		exit(98);
 	m = malloc(b);
