b3723827a073bce6d0b1c43afc0308a20720f543
Lord-Gusarov <2367@holbertonschool.com>
2020-10-19 22:06:16 -0400

    task 3 using single line typedef in header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,5 +19,7 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 /*Prints a struct of dog*/
 void print_dog(struct dog *d);
+/*anohter name ofr struc dog */
+typedef struct dog dog_t;
 #endif
 
