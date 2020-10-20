512b36b57385462d0532a2598b0a1c126c480723
Lord-Gusarov <2367@holbertonschool.com>
2020-10-19 21:04:47 -0400

    first commit of this project, adding task 0 and the README.md file

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Getting my feets wet with Structures and Typedef in C
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef __DOG_H__
+#define __DOG_H__
+/**
+ * struct dog - key info aout a dog
+ * @name: na me of the dog
+ * @age: the age of the dog
+ * @owner: the dog's servant
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
+
