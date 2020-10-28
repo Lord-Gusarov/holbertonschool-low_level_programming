056ae8bf56485a19c3921a0f54336bea211a5e39
Lord-Gusarov <2367@holbertonschool.com>
2020-10-28 16:14:05 -0400

    making it betty compliant by using 2 'printf' statements instead of splitting the string across two lines

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,6 +4,6 @@
  */
 void printBeforeMain(void)
 {
-	printf("You're beat! and yet, you must allow,\nI "
-			"bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
