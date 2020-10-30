042d08a3c47a4f49672c18236d9646b1d260e2e0
Lord-Gusarov <2367@holbertonschool.com>
2020-10-30 07:42:29 -0400

    task 5 (AKA 101) fully implemented

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,41 +3,109 @@
 #include <stdlib.h>
 
 /**
- * main - program tha multiplies two positive numbers in base 10
+ * main - program that multiplies two infinete length positive
+ * numbers in base 10
  * @ac: size of @av
- * @av:
+ * @av: arguments passed
  *
- * Return: 0 if succes, 98 if the number of arguments is incorrect
+ * Return: 0 if succes, 98 if the number of arguments or their
+ * composition is incorrect
  */
 int main(int ac, char **av)
 {
-	int n1_len = 0, n2_len = 0;
-	char ch;
+	int i1, i2, n1_len, n2_len, ip, ip2, plen = 0, *product, offset;
+	const int Z = '0';
 
-	if (ac != 3)
+	validate_arg_count(ac);
+	validate_strings_only_have_digits(av[1], av[2]);
+
+	n1_len = _strlen(av[1]), n2_len = _strlen(av[2]);
+	plen = n1_len + n2_len;
+
+	product = malloc(sizeof(int) * (plen));
+	if (product == NULL)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+	for (ip = 0; ip < plen; ip++)
+		product[ip] = 0;
 
-	for (n1_len = 0; av[1][n1_len] != '\0'; n1_len++)
+	for (i2 = n2_len - 1, offset = 0; i2 >= 0; i2--, offset++)
 	{
-		ch = av[1][n1_len];
-		if (ch < '0' || ch > '9')
+		for (i1 = n1_len - 1, ip = offset ; i1 >= 0; i1--, ip++)
 		{
-			printf("Error\n");
-			exit(98);
+			product[ip] += (av[1][i1] - Z) * (av[2][i2] - Z);
+			for (ip2 = ip; product[ip2] > 9; ip2++)
+			{
+				product[ip2 + 1] += product[ip2] / 10;
+				product[ip2] %= 10;
+			}
 		}
 	}
-	for (n2_len = 0; av[2][n2_len] != '\0'; n2_len++)
-	{
-		ch = av[2][n2_len];
-		if (ch < '0' || ch > '9')
-		{
-			printf("Error\n");
-			exit(98);
-		}
-	}
+	for (product[ip] == 0 ? (--ip) : (ip); ip >= 0; --ip)
+		_putchar(product[ip] + Z);
+	_putchar('\n');
 
+	free(product);
 	return (0);
+}
+
+/**
+ * validate_arg_count - if the arg count is wrong program exit is handled
+ * @count: arguments count
+ */
+void validate_arg_count(int count)
+{
+	if (count != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+}
+/**
+ * onlyDigits - checks if a string if compsed solely of digis
+ * @str: string to check
+ *
+ * Return: 1 if al the charcters are digits, 0 if not
+ */
+int onlyDigits(char *str)
+{
+	while (*str)
+	{
+		if (*str < '0' || *str > '9')
+			return (0);
+		str++;
+	}
+	return (1);
+}
+/**
+ * validate_strings_only_have_digits - if the string arguments passed
+ * are not made of only digits, the program exit is handled
+ * @str1: a string to check
+ * @str2: a string to check
+ */
+void validate_strings_only_have_digits(char *str1, char *str2)
+{
+	if (!onlyDigits(str1) || !onlyDigits(str2))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+}
+
+/**
+ * _strlen - calculates the length of a string
+ * *str: string to measure
+ *
+ * Return: length of the string
+ */
+int _strlen(char *str)
+{
+	int len = 0;
+
+	for (len = 0; str[len]; len++)
+		;
+
+	return (len);
 }
--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -19,5 +19,10 @@ int *array_range(int min, int max);
 /*------Advanced-----*/
 /* Task : 4 (AKA 100) */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+/*Task : 4 (AKA 101)*/
+void validate_arg_count(int count);
+void validate_strings_only_have_digits(char *str1, char *str2);
+int onlyDigits(char *str);
+int _strlen(char *str);
 #endif
 
