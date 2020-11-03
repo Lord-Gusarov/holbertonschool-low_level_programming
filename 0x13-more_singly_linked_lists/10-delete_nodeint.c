f7a1d3cb4a81611aae962d6389ef725341bede3c
Lord-Gusarov <2367@holbertonschool.com>
2020-11-03 08:08:57 -0400

    adding 'break' statement to avoid aditional changes

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -33,6 +33,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			tmp = (traverse->next)->next;
 			free(traverse->next);
 			traverse->next = tmp;
+			break;
 		}
 		traverse = traverse->next;
 		i++;
