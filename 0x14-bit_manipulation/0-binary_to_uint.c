c3c86c90790e78fefdd646b74ba0bd11da3b9bd2
Lord-Gusarov <2367@holbertonschool.com>
2020-11-05 18:04:17 -0400

    first commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string to convert
+ * Return: decimal value, 0 if it a char tha is not 0 or 1
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int sum = 0;
+	unsigned int i;
+	int len = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+
+	for (len--, i = 1; len >= 0; len--, i *= 2)
+		if (b[len] == '1')
+			sum += i;
+
+	return (sum);
+}
+
---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number to print in binary
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask = 2147483648;
+	int flag = 0;
+
+	while (mask > 0)
+	{
+		if ((n & mask) == 0)
+		{
+			if (flag)
+				_putchar('0');
+		}
+		else
+		{
+			_putchar('1');
+			flag = 1;
+		}
+		mask = mask >> 1;
+	}
+
+	if (!flag)
+		_putchar('0');
+}
+
+/*
+ * 11111111111111111111111111111111
+ * 10000000000000000000000000000000
+ */
