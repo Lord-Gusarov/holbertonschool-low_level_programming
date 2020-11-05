63e2097bba14aff06f45a6767db9ffbba8c3f143
Lord-Gusarov <2367@holbertonschool.com>
2020-11-05 18:08:45 -0400

    adding a cehck in case argument is NULL

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,6 +11,9 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int i;
 	int len = 0;
 
+	if (b == NULL)
+		return (0);
+
 	for (len = 0; b[len] != '\0'; len++)
 		if (b[len] != '0' && b[len] != '1')
 			return (0);
