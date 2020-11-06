b94b91e03daf5cf39a6758b71283d06f9f067a18
Lord-Gusarov <2367@holbertonschool.com>
2020-11-06 08:01:21 -0400

    trying now with recursion

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,31 +6,14 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 4611686018427387904;
-	int flag = 0;
-
-	while (mask > 0)
+	if (n == 0)
 	{
-		if ((n & mask) == 0)
-		{
-			if (flag)
-				_putchar('0');
-		}
-		else
-		{
-			_putchar('1');
-			flag = 1;
-		}
-		mask = mask >> 1;
-	}
-
-	if (!flag)
 		_putchar('0');
+	}
+	else
+	{
+		if ((n >> 1) != 0)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
 }
-
-/*
- * converted max unsigne long int to binary
- * 1111111111111111111111111111111111111111111111111111111111111111
- * 100000000000000000000000000000000000000000000000000000000000000
- * removed a zero cause compiler complained
- */
