454a2693d8096bf95b8999715f90ecfc0ace183d
Lord-Gusarov <2367@holbertonschool.com>
2020-11-05 20:07:50 -0400

    adding task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * flip_bits - the number of bits you would need to flip to get from one number to another
+ * @n: original number
+ * @m: destiny number
+ * Return: number of flips
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int i, nf = 0;
+	unsigned long int ii = sizeof(unsigned long int) * 8;
+
+	for (i = 0; i < ii; i++)
+	{
+		if ((m & 1) != (n & 1))
+			nf += 1;
+		n = n >> 1;
+		m = m >> 1;
+	}
+	return (nf);
+}
