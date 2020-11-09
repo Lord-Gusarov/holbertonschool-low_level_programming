7675b24e87f468f82c86c0930cc5b14cbaea5429
Lord-Gusarov <2367@holbertonschool.com>
2020-11-09 14:11:09 -0400

    making sure to close file descriptor

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,20 +13,17 @@ int create_file(const char *filename, char *text_content)
 	unsigned int size;
 	int fd;
 
-	fd = open(filename, O_TRUNC | O_WRONLY);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 00600);
 	if (fd == -1)
-	{
-		close(fd);
-		fd = open(filename, O_CREAT | O_RDWR, 00600);
-		if (fd == -1)
-			return (FAILURE);
-	}
+		return (FAILURE);
 
 	for (size = 0; text_content[size] != '\0'; size++)
 		;
 
 	if (write(fd, text_content, size) == -1)
 		return (FAILURE);
+
+	close(fd);
 
 	return (SUCCESS);
 }
