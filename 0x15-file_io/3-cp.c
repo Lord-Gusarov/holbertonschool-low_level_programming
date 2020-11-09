77459130866a75a0c9dc23ff6a6adcf73c825032
Lord-Gusarov <2367@holbertonschool.com>
2020-11-09 18:11:37 -0400

    making it betty compliant

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,7 +3,7 @@
 #define FAILURE (-1)
 /**
  * main - copies the content of one file to another
- * @args: the string aguments passed tot he program
+ * @as: the string aguments passed tot he program
  * @argc: count of rguments passed
  *
  * Return: 0 if success, if the number of argument is not the correct one,
@@ -12,7 +12,7 @@
  * if you can not create or if write to file_to fails, exit with code 99
  * if you can not close a file descriptor , exit with code 100
  */
-int main(int argc, char **args)
+int main(int argc, char **as)
 {
 	int fd_from, fd_to, cnt_rd;
 	char buf[BUF_LMT] = {'\0'};
@@ -21,23 +21,23 @@ int main(int argc, char **args)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	}
-	fd_from = open(args[1], O_RDONLY);
+	fd_from = open(as[1], O_RDONLY);
 	if (fd_from == FAILURE)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", args[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", as[1]), exit(98);
 	}
-	fd_to = open(args[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	fd_to = open(as[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == FAILURE)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", args[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", as[2]), exit(99);
 	}
 	do {
 		cnt_rd = read(fd_from, buf, BUF_LMT);
 		if (cnt_rd == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", args[1]), exit(98);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", as[1]), exit(98);
 		if (write(fd_to, buf, cnt_rd) == -1)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", args[2]), exit(99);
-	} while (cnt_rd > 0);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", as[2]), exit(99);
+	} while (cnt_rd == BUF_LMT);
 
 	if (close(fd_from) == FAILURE)
 	{
