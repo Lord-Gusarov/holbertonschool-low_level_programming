898570a23735d16b0b77d80d07ad196d41f113e5
Lord-Gusarov <2367@holbertonschool.com>
2020-11-09 14:27:23 -0400

    not changing permissions if file exists

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,9 +13,14 @@ int create_file(const char *filename, char *text_content)
 	unsigned int size;
 	int fd;
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 00600);
+	fd = open(filename, O_TRUNC | O_WRONLY);
 	if (fd == -1)
-		return (FAILURE);
+	{
+		close(fd);
+		fd = open(filename, O_CREAT | O_RDWR, 00600);
+		if (fd == -1)
+			return (FAILURE);
+	}
 
 	for (size = 0; text_content[size] != '\0'; size++)
 		;
