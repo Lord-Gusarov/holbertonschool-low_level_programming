4dd8b8758d695f565da4c35067150a924d6a8693
Lord-Gusarov <2367@holbertonschool.com>
2020-11-09 14:49:20 -0400

    rw permission for user

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (FAILURE);
 
-	fd = open(filename, O_TRUNC | O_WRONLY | O_CREAT);
+	fd = open(filename, O_TRUNC | O_WRONLY | O_CREAT, 0600);
 	if (fd == -1)
 		return (FAILURE);
 
