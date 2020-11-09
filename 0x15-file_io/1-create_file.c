fe79f599e5d36ef76ac4296db2cf86ae98cd7d3b
Lord-Gusarov <2367@holbertonschool.com>
2020-11-09 14:05:53 -0400

    firs commit task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#define SUCCESS 1
+#define FAILURE (-1)
+/**
+ * create_file - creates a file and sets the contents
+ * @filename: name for the new file
+ * @text_content: content for the new file
+ *
+ * Return: 1 on succes, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	unsigned int size;
+	int fd;
+
+	fd = open(filename, O_TRUNC | O_WRONLY);
+	if (fd == -1)
+	{
+		close(fd);
+		fd = open(filename, O_CREAT | O_RDWR, 00600);
+		if (fd == -1)
+			return (FAILURE);
+	}
+
+	for (size = 0; text_content[size] != '\0'; size++)
+		;
+
+	if (write(fd, text_content, size) == -1)
+		return (FAILURE);
+
+	return (SUCCESS);
+}
