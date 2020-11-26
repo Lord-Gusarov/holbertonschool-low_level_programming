319aaac2def6e1dc0cf4e8d0cc5162d7bb434631
Lord-Gusarov <2367@holbertonschool.com>
2020-11-26 17:16:17 -0400

    Cleaining up the Workspace

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,35 +1,37 @@
 #include "holberton.h"
-#define SUCCESS 1
-#define FAILURE (-1)
-
-
 /**
- * append_text_to_file - adds text to a file
- * @filename: name of the file to edit
- * @text_content: text to append;
- *
- * Return: 1 if success, -1 if failure
+ * append_text_to_file - appends text at the end of a file
+ * @filename: name of the file and text_content is the NULL terminated string,
+ * to add at the end of the file
+ * @text_content: text to append
+ * Return: 1 on success and -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
-	unsigned int cnt;
+	int w_out, count;
 
 	if (filename == NULL)
-		return (FAILURE);
+	return (-1);
 
-	fd = open(filename, O_APPEND | O_WRONLY);
-	if (fd == FAILURE)
-		return (FAILURE);
+	int fd = open(filename, O_WRONLY | O_APPEND);
 
-	if (text_content != NULL)
+	if (fd == -1)
 	{
-		for (cnt = 0; text_content[cnt] != '\0'; cnt++)
-			;
-		if (write(fd, text_content, cnt) == FAILURE)
-			return (FAILURE);
+		return (-1);
 	}
+	if (text_content)
+	{
 
+		count = 0;
+		while (*(text_content + count) != '\0')
+		count++;
+
+		w_out = write(fd, text_content, count);
+		if (w_out == -1)
+		{
+			return (-1);
+		}
+	}
 	close(fd);
-	return (SUCCESS);
+	return (1);
 }
