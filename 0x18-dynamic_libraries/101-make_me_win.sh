6aff68692910570a65aaa2c5cc0dc73238f1697d
Lord-Gusarov <2367@holbertonschool.com>
2020-12-14 21:12:42 -0400

    heimer version

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/Lord-Gusarov/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/liball.so
-export LD_PRELOAD=/tmp/liball.so
+wget -P /tmp/ https://github.com/Lord-Gusarov/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/librand.so
+export LD_PRELOAD=/tmp/librand.so
