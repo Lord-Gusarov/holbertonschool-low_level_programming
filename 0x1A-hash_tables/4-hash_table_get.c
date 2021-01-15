101de6f967d831a1ab0c01706d6c30158fc12629
Lord-Gusarov <2367@holbertonschool.com>
2021-01-15 19:40:35 -0400

    validating arguments are not NULL

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,7 +11,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int idx;
 	hash_node_t *trv = NULL;
 
-	if (strcmp(key, "") == 0)
+	if (!ht || !key || strcmp(key, "") == 0)
 		return (NULL);
 
 	idx = key_index((const unsigned char *) key, ht->size);
