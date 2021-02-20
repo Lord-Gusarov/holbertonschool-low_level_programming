ec81c1fc550eac4d61b3f79fe26a61fc62d3aff0
Lord-Gusarov <70659799+Lord-Gusarov@users.noreply.github.com>
2021-02-19 20:51:36 -0400

    If the land is in a border count the perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -10,15 +10,6 @@ def island_perimeter(grid):
     Args:
         grid (2D list): 1's and 0's
     """
-    if type(grid) is not list:
-        return 0
-    for x in grid:
-        if type(x) is not list:
-            return 0
-        for num in x:
-            if type(num) is not int:
-                return 0
-
     p = 0
     r_max = len(grid)
     c_max = len(grid[0])
@@ -26,12 +17,12 @@ def island_perimeter(grid):
     for r in range(r_max):
         for c in range(c_max):
             if grid[r][c] == 1:
-                if r + 1 < r_max and grid[r + 1][c] == 0:
+                if r == r_max - 1 or grid[r + 1][c] == 0:
                     p += 1
-                if r - 1 >= 0 and grid[r - 1][c] == 0:
+                if r == 0 or grid[r - 1][c] == 0:
                     p += 1
-                if c + 1 < c_max and grid[r][c + 1] == 0:
+                if c == c_max - 1 or grid[r][c + 1] == 0:
                     p += 1
-                if c - 1 >= 0 and grid[r][c - 1] == 0:
+                if c == 0 or grid[r][c - 1] == 0:
                     p += 1
     return p
